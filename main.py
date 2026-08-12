efd1ee251be9a9f288080115c9f9a1994336c16c
Pranav Pardeshi <im.pranav.pardeshi@gmail.com>
2026-08-13 01:04:13 +0530

    feat: add dummy data for tasting on creation of table - create Campaign model first

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from contextlib import asynccontextmanager
+from datetime import datetime, timezone
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+
+class Campaign(SQLModel, table=True):
+    campaign_id: int | None = Field(default=None, primary_key=True)
+    name: str | None = Field(index=True)
+    due_date: datetime | None = Field(default=None, index=True)
+    created_at: datetime | None = Field(default=lambda: datetime.now(timezone.utc), nullable=True,  index=True)
 
 
 # Initialize database
@@ -25,6 +32,13 @@ session_dp = Annotated[Session, Depends(get_session)]
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
+    with Session(engine) as session:
+        if not session.exec(select(Campaign.first())):
+            session.add_all([
+                Campaign(name="Summer Launch", due_date=datetime.now()),
+                Campaign(name="Winter Launch", due_date=datetime.now()),
+                Campaign(name="Black Friday", due_date=datetime.now())
+            ])
     yield
 
 app = FastAPI(root_path="/api/v1", lifespan=lifespan)
